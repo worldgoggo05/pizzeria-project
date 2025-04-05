309a2076dcb6ec6915b6b2b5381b3c81948b9665
Alex <worldgoggo05@gmail.com>
2025-04-05 13:34:37 +0900

    feat:basic UI of cal ai created

--- Pizzeria-Front/src/app/screens/homePage/Advertisement.tsx
+++ Pizzeria-Front/src/app/screens/homePage/Advertisement.tsx
@@ -3,16 +3,18 @@ import React from "react";
 export default function Advertisement() {
   return (
     <div className="ads-restaurant-frame">
-      <video
-        className="ads-video"
-        autoPlay={true}
-        loop
-        muted
-        playsInline
-        data-video-media=""
-      >
-        <source type="video/mp4" src="video/pizads.mp4" />
-      </video>
+      <div className="video-container">
+        <video
+          className="ads-video"
+          autoPlay={true}
+          loop
+          muted
+          playsInline
+          data-video-media=""
+        >
+          <source type="video/mp4" src="video/pizads.mp4" />
+        </video>
+      </div>
     </div>
   );
 }
