521be1016f19f0203ac92015d5d3a40007d610d6
Alex <worldgoggo05@gmail.com>
2025-04-06 22:54:20 +0900

    feat : Backend Login and Sign-Up ui improved

--- Pizzeria-Back/Tasks/P-Task - Solutions.js
+++
@@ -1,7 +0,0 @@
-// P-TASK:
-
-// Shunday function yozing, u object qabul qilsin va arrayni object arrayga otkazib arrayni qaytarsin qaytarsin.
-// MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
-
-const objectToArray = (obj) => Object.entries(obj);
-console.log(objectToArray({a:10,b:20}))
